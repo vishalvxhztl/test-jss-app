36d3292306a1b769e335634f9b14e21fff3238d9
vishalvx <dev.vishalprajapati.24@gmail.com>
2024-03-01 15:43:11 +0530

    remove tailwind variant,add clsx.

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -1,27 +1,19 @@
 //--- Dependencies
 import { getPublicUrl } from '@sitecore-jss/sitecore-jss-nextjs';
-import { tv } from 'tailwind-variants';
 
 //--- Helpers
 import Button from 'components/helpers/Buttons';
+import { cn } from 'lib/utils/cn';
 
 // Prefix public assets with a public URL to enable compatibility with Sitecore Experience Editor.
 // If you're not supporting the Experience Editor, you can remove this.
 const publicUrl = getPublicUrl();
 
-const navigation = tv({
-  slots: {
-    navbar: 'flex justify-between p-4 bg-slate-400',
-    navWrapper: 'flex items-center gap-8',
-  },
-});
-
 const Navigation = (): JSX.Element => {
-  const { navbar, navWrapper } = navigation();
   return (
-    <div className={navbar()}>
+    <div className={cn('flex justify-between p-4 bg-slate-400')}>
       <nav>
-        <ul className={navWrapper()}>
+        <ul className={cn('flex items-center gap-8')}>
           <li>
             <a href="https://sitecore.com">
               <img src={`${publicUrl}/sc_logo.svg`} alt="Sitecore" />
