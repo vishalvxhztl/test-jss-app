071c21e8d36dd1e2efde14d1fdcc129bd8d7eeb2
vishalvx <dev.vishalprajapati.24@gmail.com>
2024-03-01 16:34:24 +0530

    update ts dependency, add tailwind variant.

--- src/components/helpers/Buttons/index.tsx
+++ src/components/helpers/Buttons/index.tsx
@@ -1,12 +1,38 @@
-import { cn } from 'lib/utils/cn';
+import { tv } from 'tailwind-variants';
 
 //--- Type Definations
 export type buttonProps = {
   text: string;
 };
 
+//--- Style Defination
+const button = tv({
+  base: 'font-medium bg-blue-500 text-white rounded-full active:opacity-80',
+  variants: {
+    color: {
+      primary: 'bg-blue-500 text-white',
+      secondary: 'bg-purple-500 text-white',
+    },
+    size: {
+      sm: 'text-sm',
+      md: 'text-base',
+      lg: 'px-4 py-3 text-lg',
+    },
+  },
+  compoundVariants: [
+    {
+      size: ['sm', 'md'],
+      class: 'px-3 py-1',
+    },
+  ],
+  defaultVariants: {
+    size: 'md',
+    color: 'primary',
+  },
+});
+
 const Button = ({ text }: buttonProps) => {
-  return <button className={cn('bg-blue-400', 'text-black')}>{text}</button>;
+  return <button className={button()}>{text}</button>;
 };
 
 export default Button;
